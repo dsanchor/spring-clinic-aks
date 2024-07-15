0fcefd5b735becd6e28539c65eb32ab4bd7c4f8f
dsanchor <dsanchoruiz@microsoft.com>
2024-07-15 09:41:02 +0200

    chore: Remove unused files and configurations

--- src/spring-petclinic-messaging-emulator/src/main/resources/schema-hsqldb.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE visitrequests IF EXISTS;
-CREATE TABLE visitrequests (
-  id INTEGER IDENTITY PRIMARY KEY,
-  pet_id INTEGER NOT NULL,
-  message VARCHAR(2048),
-  response VARCHAR(2048),
-  accepted INT
-);
